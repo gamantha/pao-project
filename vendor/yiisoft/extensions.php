cdc5e618408668e31cff2a8b4519894c026021ff
Nirwan Praesta <nirwana@xl.co.id>
2017-08-20 19:29:03 +0700

    update profile & radar

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,15 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'gamantha/pao-project' => 
+  array (
+    'name' => 'gamantha/pao-project',
+    'version' => 'dev-nirwan',
+    'alias' => 
+    array (
+      '@vendor/gamantha/pao/project' => $vendorDir . '/gamantha/pao-project',
+    ),
+  ),
+);
