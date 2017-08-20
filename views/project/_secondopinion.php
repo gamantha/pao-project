1e8d5c0c21ac0785b1c35445455511b7393c75e3
renowijoyo <renowijoyo@gmail.com>
2017-08-20 16:12:15 +0700

    +_secondopinion +_supervisor

---
+++ views/project/_secondopinion.php
@@ -0,0 +1,78 @@
+<?php
+use vendor\gamantha\pao\project\models\Project;
+use vendor\gamantha\pao\project\models\ProjectMeta;
+use vendor\gamantha\pao\project\models\ProjectActivity;
+use vendor\gamantha\pao\project\models\ProjectActivityMeta;
+use common\modules\profile\models\Profile;
+use common\modules\profile\models\ProfileMeta;
+use yii\data\ActiveDataProvider;
+//use Yii;
+use Yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+
+?>
+<hr/>
+=== Second Opinion assessor PAGE ===
+<br/>
+apa yang mau dilihat Second opinion disini?
+<br/>
+- semua aktifitas di group berdasarkan assessor
+
+
+<?php
+
+$profile_model = Profile::find()->andWhere(['user_id' => Yii::$app->user->id])->One();
+
+            $distinct_assessor_activities_model = ProjectActivityMeta::find()
+                ->andWhere(['type' => 'general'])
+                ->andWhere(['key' => 'second_opinion'])
+                ->andWhere(['value' => $profile_model->id])
+                ->asArray()->All();
+
+            $assessor_activity_ids = ArrayHelper::getColumn($distinct_assessor_activities_model, 'project_activity_id');
+
+
+            $activityQuery = ProjectActivity::find()
+            ->andWhere(['project_id' => $_GET['id']])
+            ->andWhere(['in','id',$assessor_activity_ids]);
+
+			$activityDataProvider = new ActiveDataProvider([
+			    'query' => $activityQuery,
+			    'pagination' => [
+			        'pageSize' => 10,
+			    ],
+			    'sort' => [
+			        'defaultOrder' => [
+			//            'created_at' => SORT_DESC,
+			  //          'title' => SORT_ASC, 
+			        ]
+			    ],
+			]);
+
+
+
+			 echo GridView::widget([
+			        'dataProvider' => $activityDataProvider,
+			       // 'filterModel' => $searchModel,
+			        'columns' => [
+			            //['class' => 'yii\grid\SerialColumn'],
+
+			            //'id',
+			            [
+			            	'label' => 'Activity',
+			            	'attribute' => 'id',
+			            	'format' => 'raw',
+			            	'value' => function($data){
+			            		$link = Html::a($data->id, ['activity/view', 'id' => $data->id], ['class' => 'profile-link']);
+			            		return $link;
+			            	}
+			            ],
+            	
+
+			        ],
+			    ]);
+
+
+?>
